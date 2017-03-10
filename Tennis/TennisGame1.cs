ed3f10ce38d956518a5f69029645b343cac68d27
valenti <valenti@VALENTI-LAPTOP>
2017-03-10 14:17:21 -0800

    Refactor out ConvertScoreToString method

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Tennis
 {
     class TennisGame1 : ITennisGame
@@ -48,26 +50,35 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
+                    if (i == 1)
                     {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
+                        tempScore = m_score1;
                     }
+                    else
+                    {
+                        score += "-";
+                        tempScore = m_score2;
+                    }
+                    score += ConvertScoreToString(tempScore);
                 }
             }
             return score;
+        }
+
+        private static string ConvertScoreToString(int score)
+        {
+            switch (score)
+            {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+            }
+            throw new ArgumentException("score");
         }
     }
 }
